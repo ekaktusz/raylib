216a1ec02d1128cea4a1b413afe8979702592d4b
Ekart Csaba <csaba.ekart@gmail.com>
2022-12-03 17:11:09 +0100

    add premake

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "Raylib"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.c",
+	}
+
+	includedirs
+	{
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
